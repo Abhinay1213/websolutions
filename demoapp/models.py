fcd11ac8d2525e2bffe54cf8f455a38d2a2d8805
Abhinay1213 <158468270+Abhinay1213@users.noreply.github.com>
2024-04-11 04:47:53 +0530

    Update models.py

--- demoapp/models.py
+++ demoapp/models.py
@@ -77,3 +77,18 @@ class Wishlist(models.Model):
 
     def __str__(self):
         return f"{self.user.username}'s Wishlist"
+
+class Interest(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer_name = models.CharField(max_length=100, default='')
+    customer_email = models.EmailField(default='')
+    message = models.TextField(default='')
+    created_at = models.DateTimeField(default=timezone.now)
+    interested_users = models.ManyToManyField(User, related_name='interested_products')
+
+    class Meta:
+        unique_together = ('user', 'product')
+
+    def __str__(self):
+        return f"Interest for {self.product} by {self.customer_name}"
